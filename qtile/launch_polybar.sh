25fbd83853339b66d7cbee3680f942dfde2a309b
NicoDevStuff <NicoG2008@gmx.de>
2023-03-19 14:01:48 +0100

    removed qtile, because i dont use it

--- qtile/launch_polybar.sh
+++
@@ -1,9 +0,0 @@
-killall polybar
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c ~/.config/leftwm/themes/gruvbox/config_polybar.ini &
-  done
-else
-	polybar -c ~/.config/leftwm/themes/gruvbox/config_polybar.ini &
-fi
