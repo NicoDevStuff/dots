065db752886fc7dddcdaa5fc9a5c9c70bd648fd0
NicoDevStuff <NicoG2008@gmx.de>
2023-06-28 10:35:27 +0200

    switched shell to zsh, now improving nvim

--- polybar/scripts/caps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-if [ $(xset -q | sed -n 's/^.*Caps Lock:\s*\(\S*\).*$/\1/p') = "on" ]; then
-	echo "⬆"
-else
-	echo "⬇"
-fi
---
+++ polybar/scripts/weather.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ~/.config/polybar/scripts/weather.py
